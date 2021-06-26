dbf2131d017ddc3330b19fdf7687f243c8d69833
ChingFeng <hcforsch@gmail.com>
2021-06-26 17:08:43 +0800

    init Project

---
+++ Reflection.CopyObject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reflection.CopyObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
